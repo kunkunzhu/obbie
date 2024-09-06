fc0d0fef5b018a017878efa6a1c7e67eda00d6e3
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-09-06 14:19:33 -0400

    init tailwind themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,30 @@
+/** @format */
+
 import type { Config } from "tailwindcss";
+const { nextui } = require("@nextui-org/react");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
-  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--off-white)",
+        primary: "var(--off-black)",
+        secondary: "var(--grey)",
+      },
+      fontFamily: {
+        text: ['"Outfit"', ...defaultTheme.fontFamily.sans],
+        title: ['"Anybody"', ...defaultTheme.fontFamily.sans],
+      },
+      dropShadow: {
+        entry: "2px 2px 10px rgba(0, 0, 0, 0.05)",
+      },
+    },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 } satisfies Config;
