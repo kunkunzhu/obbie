4f2a265cb3b3930f51cfba4a414914e3db4baf36
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-09-06 14:19:48 -0400

    create page views

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,20 @@
+/** @format */
+
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useNavigation,
 } from "@remix-run/react";
 import "./tailwind.css";
+import { hobbiesData } from "./lib/example-data";
+import { HobbyI } from "./types";
+import NavHeader from "./components/header/NavHeader";
+import SidebarNav from "./components/sidebar/SidebarNav";
+import { NextUIProvider } from "@nextui-org/react";
+import { cn } from "./lib/utils";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -13,18 +22,50 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Anybody:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet"
+        />
         <Meta />
         <Links />
       </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
+      <body className="flex max-w-screen max-h-screen overflow-hidden font-text bg-background text-primary">
+        <NextUIProvider>
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+        </NextUIProvider>
       </body>
     </html>
   );
 }
 
 export default function App() {
-  return <Outlet />;
+  const exampleHobbiesData = hobbiesData as HobbyI[];
+  const navigation = useNavigation();
+
+  return (
+    <div>
+      <NavHeader />
+      <div className="flex">
+        <div className="p-10 w-[20vw] flex flex-col justify-center">
+          <SidebarNav hobbies={exampleHobbiesData} />
+        </div>
+        <div
+          className={cn(
+            "w-[80vw] px-10 py-0 ml-25",
+            navigation.state === "loading" && "blur"
+          )}
+        >
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ app/routes/($selection).tsx
@@ -0,0 +1,77 @@
+/** @format */
+
+// import TimelineTracker from "../components/tracker/tracker-timeline";
+import {
+  Outlet,
+  useLoaderData,
+  useParams,
+  useSearchParams,
+} from "@remix-run/react";
+
+import { ActionFunctionArgs, json, LoaderFunctionArgs } from "@remix-run/node";
+import { HobbyMutationI } from "~/types";
+import TimelineTracker from "~/components/tracker/TimelineTracker";
+import CalTracker from "~/components/tracker/CalTracker";
+import { createHobbyEntry, getHobbyEntries } from "~/lib/data";
+import { getEmojiDict, timeTable } from "~/lib/example-data";
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  // CREATE ACTION (move to more appropriate location later)
+
+  const formData = await request.formData();
+  const formEntry = Object.fromEntries(formData);
+
+  const dateString = formEntry.date as string;
+  const projectsString = formEntry.projects as string;
+
+  const entry: HobbyMutationI = {
+    hobby: formEntry.hobby as string,
+    description: formEntry.description as string,
+    projects: projectsString ? projectsString.split(",") : [],
+    date: {
+      year: parseInt(dateString.substring(0, 4)),
+      month: parseInt(dateString.substring(5, 7)),
+      day: parseInt(dateString.substring(8, 10)),
+    },
+    dateStr: dateString.substring(0, 10),
+    star: formEntry.star ? true : false,
+  };
+
+  return await createHobbyEntry(entry);
+};
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  const entries = params
+    ? await getHobbyEntries(params.selection)
+    : await getHobbyEntries();
+  if (!entries) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  return json({ entries });
+};
+
+export default function Home() {
+  const [searchParams] = useSearchParams();
+  const { entries } = useLoaderData<typeof loader>();
+  const emojiDict = getEmojiDict();
+  const params = useParams();
+
+  return (
+    <div>
+      <div id="pt-10 pb-20" className="max-h-[25vh]">
+        <div>
+          <CalTracker
+            months={timeTable.months}
+            days={timeTable.days}
+            entries={entries}
+            hobby={params.selection ? params.selection : "all"}
+          />
+        </div>
+      </div>
+      <div id="pt-10 pb-20" className="max-h-[65vh]">
+        <TimelineTracker entries={entries} emojiDict={emojiDict} />
+        <Outlet />
+      </div>
+    </div>
+  );
+}
