45703e367eee9988324f28d1518272de3be0c260
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-09-16 17:36:44 -0400

    create session storage

---
+++ app/services/session.server.ts
@@ -0,0 +1,94 @@
+/** @format */
+
+import { createCookieSessionStorage, redirect } from "@remix-run/node";
+import invariant from "tiny-invariant";
+import { getUserById, User } from "./user.server";
+
+invariant(process.env.SESSION_SECRET, "SESSION_SECRET must be set");
+
+export const sessionStorage = createCookieSessionStorage({
+  cookie: {
+    name: "__session",
+    httpOnly: true,
+    path: "/",
+    sameSite: "lax",
+    maxAge: 60 * 60 * 24 * 30,
+    secrets: [process.env.SESSION_SECRET],
+    secure: process.env.NODE_ENV === "production",
+  },
+});
+
+const USER_SESSION_KEY = "userId";
+
+export async function getUserSession(request: Request) {
+  const cookie = request.headers.get("Cookie");
+  return sessionStorage.getSession(cookie);
+}
+
+export async function getUserId(
+  request: Request
+): Promise<User["id"] | undefined> {
+  const session = await getUserSession(request);
+  const userId = session.get(USER_SESSION_KEY);
+  return userId;
+}
+
+export async function getUser(request: Request) {
+  const userId = await getUserId(request);
+  if (userId === undefined || typeof userId !== "string") return null;
+
+  try {
+    const user = await getUserById(userId);
+    return user;
+  } catch {
+    throw logout(request);
+  }
+}
+
+export async function logout(request: Request) {
+  const session = await getUserSession(request);
+  return redirect("/sign-in", {
+    headers: {
+      "Set-Cookie": await sessionStorage.destroySession(session),
+    },
+  });
+}
+
+export async function requireUserId(
+  request: Request
+  // redirectTo: string = new URL(request.url).pathname
+) {
+  const userId = await getUserId(request);
+  if (!userId) {
+    // const searchParams = new URLSearchParams([["redirectTo", redirectTo]]);
+    throw redirect("/sign-in");
+  }
+  return userId;
+}
+
+// export async function requireUser(request: Request) {
+//   const userId = await requireUserId(request);
+
+//   const user = await getUserById(userId);
+//   if (user) return user;
+
+//   throw await logout(request);
+// }
+
+export async function createUserSession({
+  // request,
+  userId,
+  redirectTo = "/home",
+}: {
+  // request: Request;
+  userId: string;
+  redirectTo?: string;
+}) {
+  const session = await sessionStorage.getSession();
+  session.set(USER_SESSION_KEY, userId);
+  return redirect(redirectTo, {
+    headers: {
+      "Set-Cookie": await sessionStorage.commitSession(session),
+    },
+  });
+}
