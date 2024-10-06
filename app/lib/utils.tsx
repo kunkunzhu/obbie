eef0b986b953c54d8b7391e1f9edc6ea42306788
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-10-06 14:32:01 -0400

    add user-related util functions

--- app/lib/utils.tsx
+++ app/lib/utils.tsx
@@ -3,6 +3,9 @@
 import { DateI } from "../types";
 import { twMerge } from "tailwind-merge";
 import { clsx, type ClassValue } from "clsx";
+import { useMatches } from "@remix-run/react";
+import { useMemo } from "react";
+import { User } from "@prisma/client";
 
 export function dateIntoYear(date: DateI) {
   let dayCount = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
@@ -14,4 +17,46 @@ export function dateIntoYear(date: DateI) {
 
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
+}
+
+export function useMatchesData(
+  id: string
+): Record<string, unknown> | undefined {
+  const matchingRoutes = useMatches();
+  const route = useMemo(
+    () => matchingRoutes.find((route) => route.id === id),
+    [matchingRoutes, id]
+  );
+  return route?.data as Record<string, unknown>;
+}
+
+function isUser(user: unknown): user is User {
+  return (
+    user != null &&
+    typeof user === "object" &&
+    "email" in user &&
+    typeof user.email === "string"
+  );
+}
+
+export function useOptionalUser(): User | undefined {
+  const data = useMatchesData("root");
+  if (!data || !isUser(data.user)) {
+    return undefined;
+  }
+  return data.user;
+}
+
+export function useUser(): User {
+  const maybeUser = useOptionalUser();
+  if (!maybeUser) {
+    throw new Error(
+      "No user found in root loader, but user is required by useUser. If user is optional, try useOptionalUser instead."
+    );
+  }
+  return maybeUser;
+}
+
+export function validateEmail(email: unknown): email is string {
+  return typeof email === "string" && email.length > 3 && email.includes("@");
 }
