8fc69d8b91c9e7c0db172e760836578d96b435e9
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-10-16 20:12:56 -0400

    add server functions for adding hobby onto user profile

--- app/services/user.server.ts
+++ app/services/user.server.ts
@@ -4,6 +4,8 @@ import bcrypt from "bcryptjs";
 import { prisma } from "./prisma.server";
 import type { User } from "@prisma/client";
 import { UserRegistration } from "./type.server";
+import { HobbyI } from "~/types";
+import { json } from "@remix-run/node";
 
 export type { User } from "@prisma/client";
 
@@ -27,4 +29,70 @@ export async function getUserById(userId: string) {
   })) as User;
   return user;
 }
-// }
+
+export async function markUserProfileAsComplete(userId: string) {
+  const completedProfile = await prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      profileComplete: true,
+    },
+  });
+
+  if (!completedProfile) {
+    const errorMsg =
+      "Oops! Something went wrong while marking the profile as complete.";
+    console.log(errorMsg);
+    return { error: errorMsg, status: 400 };
+  }
+
+  console.log(`The user ${userId}'s profile is now complete!`);
+
+  return json({ status: 200 });
+}
+
+export async function addUserHobbies(hobbies: HobbyI[], userId: string) {
+  if (!userId) {
+    const errorMsg = "No user found.";
+    console.log(errorMsg);
+    return { error: errorMsg, status: 400 }; // TO DO: change to more detailed HTTP error codes
+  }
+
+  const newHobbies = hobbies.map(async (hobby) => {
+    const exists = await prisma.hobby.findFirst({
+      where: {
+        name: hobby.name,
+        emoji: hobby.emoji,
+        userId: userId,
+      },
+    });
+
+    // TO DO: add more robust error handling (e.g. when hobby already exists, other errors)
+    if (!exists) {
+      const updatedHobby = await prisma.hobby.create({
+        data: {
+          name: hobby.name,
+          emoji: hobby.emoji,
+          user: {
+            connect: {
+              id: userId,
+            },
+          },
+        },
+      });
+    } else {
+      console.log(`Hobby ${hobby.emoji} already exists!`);
+    }
+  });
+
+  if (!newHobbies) {
+    const errorMsg = "No user found.";
+    console.log(errorMsg);
+    return { error: errorMsg, status: 400 };
+  }
+
+  console.log("Hobbies successfully added!");
+
+  return json({ status: 200 });
+}
