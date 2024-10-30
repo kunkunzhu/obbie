0098d52ec1560f3ed78caf67b170243bf2da1aa6
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-10-30 15:22:34 -0400

    update userheader

--- app/components/auth/UserHeader.tsx
+++ app/components/auth/UserHeader.tsx
@@ -1,8 +1,15 @@
 /** @format */
 
-import { Avatar, Button, Tooltip } from "@nextui-org/react";
+import {
+  Avatar,
+  Button,
+  Divider,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
 import type { User } from "@prisma/client";
-import { useFetcher } from "@remix-run/react";
+import { NavLink, useFetcher } from "@remix-run/react";
 
 function SignOutButton() {
   const fetcher = useFetcher();
@@ -20,10 +27,26 @@ export default function UserHeader(user: { user: User }) {
   return (
     <div className="flex gap-4 align-middle absolute top-0 right-0 px-12 py-8">
       {/* TO DO: allow customizable avatars */}
-      <Tooltip content={user.user.username}>
-        <Avatar isBordered />
-      </Tooltip>
-      <SignOutButton />
+      <Popover placement="bottom" showArrow={true} className="mt-2">
+        <PopoverTrigger className="cursor-pointer">
+          <Avatar isBordered />
+        </PopoverTrigger>
+        <PopoverContent className="flex flex-col gap-2 py-2 min-w-[120px] max-w-[150px]">
+          <div className="flex flex-col text-center">
+            <span className="opacity-50 text-xs">Logged in as</span>
+            <span className="px-2 truncate text-medium">
+              {user.user.username}
+            </span>
+          </div>
+          <Divider />
+          <div className="gap-2 flex flex-col">
+            <NavLink to="/etc/profile">
+              <Button>Profile</Button>
+            </NavLink>
+            <SignOutButton />
+          </div>
+        </PopoverContent>
+      </Popover>
     </div>
   );
 }
